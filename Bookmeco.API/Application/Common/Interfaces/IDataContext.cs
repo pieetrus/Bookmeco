846c942d2ff8d1c1cb935a8b4b9df64e5fdccd76
pieetrus <pietrusjakub@gmail.com>
2021-03-11 16:52:46 +0100

    Intall EF Core in Application layer

--- Bookmeco.API/Application/Common/Interfaces/IDataContext.cs
+++ Bookmeco.API/Application/Common/Interfaces/IDataContext.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.Common.Interfaces
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
 {
     public interface IDataContext
     {
-
+         DbSet<ServiceProvider> ServiceProviders { get; set; }
+         DbSet<ServiceCategory> ServiceCategories { get; set; }
     }
 }
