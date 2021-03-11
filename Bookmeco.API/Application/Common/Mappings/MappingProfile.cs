90f55011da98a151540881b593875202cc4535ae
pieetrus <pietrusjakub@gmail.com>
2021-03-11 16:50:34 +0100

    Configure mapper and mediatR

---
+++ Bookmeco.API/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
