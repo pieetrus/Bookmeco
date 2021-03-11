11342738dd15ee0b0fd328240549c6f011c67825
pieetrus <pietrusjakub@gmail.com>
2021-03-11 16:22:01 +0100

    Add initial migration, change sqlite.db file location

---
+++ Bookmeco.API/Infrastructure/Persistence/Migrations/20210311141940_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServiceCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceProviders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceProviders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceCategoryServiceProvider",
+                columns: table => new
+                {
+                    ServiceCategoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ServiceProvidersId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceCategoryServiceProvider", x => new { x.ServiceCategoryId, x.ServiceProvidersId });
+                    table.ForeignKey(
+                        name: "FK_ServiceCategoryServiceProvider_ServiceCategories_ServiceCategoryId",
+                        column: x => x.ServiceCategoryId,
+                        principalTable: "ServiceCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ServiceCategoryServiceProvider_ServiceProviders_ServiceProvidersId",
+                        column: x => x.ServiceProvidersId,
+                        principalTable: "ServiceProviders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceCategoryServiceProvider_ServiceProvidersId",
+                table: "ServiceCategoryServiceProvider",
+                column: "ServiceProvidersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceCategoryServiceProvider");
+
+            migrationBuilder.DropTable(
+                name: "ServiceCategories");
+
+            migrationBuilder.DropTable(
+                name: "ServiceProviders");
+        }
+    }
+}
