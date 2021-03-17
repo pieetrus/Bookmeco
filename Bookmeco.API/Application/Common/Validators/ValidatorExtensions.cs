54fb236434dc0c9bd2f2baa7477972005494463e
pieetrus <pietrusjakub@gmail.com>
2021-03-17 18:44:30 +0100

    Add register request validator

---
+++ Bookmeco.API/Application/Common/Validators/ValidatorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Validators
+{
+    public static class ValidatorExtensions
+    {
+        public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var options = ruleBuilder
+                    .NotEmpty()
+                    .MinimumLength(3).WithMessage("Password must be at least 3 characters")
+                //.Matches("[A-Z]").WithMessage("Password must contain at least 1 uppercase character")
+                //.Matches("[a-z]").WithMessage("Password must contain at least 1 lowercase character")
+                //.Matches("[0-9]").WithMessage("Password must contain at least 1 number")
+                //.Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least 1 non alphanumeric character")
+                ;
+            return options;
+        }
+    }
+}
---
+++ Bookmeco.API/Application/Users/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.Users.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).Password();
+            RuleFor(x => x.Username).NotEmpty();
+        }
+    }
+}
